bda5de2659cb8141fd8eab524fabc3f881f3e07e
Biny19 <binalfew.tadele@gmail.com>
2023-06-17 12:52:13 -0700

    It's all about 0x0F. Python - Object-relational mapping project

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
+
 import sys
 from model_state import Base, State
 from model_city import City
@@ -8,12 +7,26 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+def listStateObj():
+    """
+    Class that lists all cities by state in the given database.
+    The equivalent to using JOIN in SQL, this is done using sqlalchemy
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
+    my_session = Session()
+
+    city_state = my_session.query(State, City).filter(
+        City.state_id == State.id).order_by(City.id).all()
+
+    for x in city_state:
+        print("{}: ({}) {}".format(x.State.name, x.City.id, x.City.name))
+
+    my_session.close()
+
+if __name__ == '__main__':
+    listStateObj() 
