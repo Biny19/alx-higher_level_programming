7ed03c94051f849c9c85f3b09ad45cbdaf475526
Biny19 <binalfew.tadele@gmail.com>
2023-06-17 13:01:33 -0700

    It's all about 0x0F. Python - Object-relational mapping project

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-from model_state import Base, State
+"""
+Contains the class definition of a City
+"""
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
 
 class City(Base):
-    """ City class """
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False, unique=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
